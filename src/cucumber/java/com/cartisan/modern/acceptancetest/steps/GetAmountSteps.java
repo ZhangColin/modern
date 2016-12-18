2eb1530f0fa06eb56d7013a12f5d7593e159dfe6
colin <zhang_colin@163.com>
2016-12-18 22:08:45 +0800

    get amount with no bugget for any month. witch no implementation for steps

---
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/GetAmountSteps.java
@@ -0,0 +1,15 @@
+package com.cartisan.modern.acceptancetest.steps;
+
+
+import cucumber.api.java8.En;
+
+public class GetAmountSteps implements En {
+    {
+        Given("^no budget for any month$", () -> {
+        });
+        When("^get amount of period from \"([^\"]*)\" to \"([^\"]*)\"$", (String arg1, String arg2) -> {
+        });
+        Then("^the amount is (\\d+)$", (Integer arg1) -> {
+        });
+    }
+}
