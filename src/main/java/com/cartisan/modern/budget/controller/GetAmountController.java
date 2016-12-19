67248d5197580eaf6e5546cc45d99c507f0c8406
colin <zhang_colin@163.com>
2016-12-19 20:08:58 +0800

    controller now gets amount from monthly budget planner

--- src/main/java/com/cartisan/modern/budget/controller/GetAmountController.java
+++ src/main/java/com/cartisan/modern/budget/controller/GetAmountController.java
@@ -1,12 +1,29 @@
 package com.cartisan.modern.budget.controller;
 
+import com.cartisan.modern.budget.MonthlyBudgetPlanner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class GetAmountController {
+    private final MonthlyBudgetPlanner planner;
+
+    @Autowired
+    public GetAmountController(MonthlyBudgetPlanner planner) {
+        this.planner = planner;
+    }
+
     @RequestMapping("/get_amount")
-    public String getAmount() {
+    public String getAmount(
+            @RequestParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd")Date startDate,
+            @RequestParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
+        planner.getAmount(startDate, endDate);
+
         return "get_amount";
     }
 }
--- src/test/java/com/cartisan/modern/budget/controller/GetAmountControllerTest.java
+++ src/test/java/com/cartisan/modern/budget/controller/GetAmountControllerTest.java
@@ -1,16 +1,42 @@
 package com.cartisan.modern.budget.controller;
 
 
+import com.cartisan.modern.budget.MonthlyBudgetPlanner;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class GetAmountControllerTest {
 
+    MonthlyBudgetPlanner mockPlanner = mock(MonthlyBudgetPlanner.class);
+    GetAmountController controller = new GetAmountController(mockPlanner);
+
+    Date startDate = parseDate("2016-07-01");
+    Date endDate = parseDate("2016-07-10");
+
     @Test
     public void go_to_get_amount_page() {
-        GetAmountController controller = new GetAmountController();
-        assertEquals("get_amount", controller.getAmount());
+        assertEquals("get_amount", controller.getAmount(startDate, endDate));
+    }
+
+    @Test
+    public void get_amount_from_monthly_budget_planner(){
+        controller.getAmount(startDate, endDate);
+
+        verify(mockPlanner).getAmount(startDate, endDate);
+    }
+
+    public GetAmountControllerTest() throws ParseException {
+    }
+
+    private Date parseDate(String source) throws ParseException {
+        return new SimpleDateFormat("yyyy-MM-dd").parse(source);
     }
 
 }
