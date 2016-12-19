6f3994f5f9f51d569fb248c639df65862a56cbb9
colin <zhang_colin@163.com>
2016-12-19 17:54:25 +0800

    get amount with no budget for any month. Passing by hard code the amount

--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/GetAmountSteps.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/GetAmountSteps.java
@@ -2,10 +2,11 @@ package com.cartisan.modern.acceptancetest.steps;
 
 
 import cucumber.api.java8.En;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GetAmountSteps implements En {
     WebDriver driver;
@@ -17,8 +18,9 @@ public class GetAmountSteps implements En {
             driver = new FirefoxDriver();
             driver.get("http://localhost:8080/get_amount?startDate=" + startDate + "&endDate=" + endDate);
         });
-        Then("^the amount is (\\d+)$", (Integer arg1) -> {
-            assertEquals("Modern", driver.getTitle());
+        Then("^the amount is (\\d+)$", (Integer amount) -> {
+            String bodyText = driver.findElement(By.tagName("body")).getText();
+            assertTrue(bodyText.contains(String.valueOf(amount)));
             driver.close();
         });
     }
