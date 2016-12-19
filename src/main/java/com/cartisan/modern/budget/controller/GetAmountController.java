54f878e7a59bdf2df2f20be9f2c5140cfcab13ae
colin <zhang_colin@163.com>
2016-12-19 19:51:38 +0800

    add  get amount controller and keep current page flow unchanged but wip scenario is still failling

---
+++ src/main/java/com/cartisan/modern/budget/controller/GetAmountController.java
@@ -0,0 +1,12 @@
+package com.cartisan.modern.budget.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GetAmountController {
+    @RequestMapping("/get_amount")
+    public String getAmount() {
+        return "get_amount";
+    }
+}
---
+++ src/test/java/com/cartisan/modern/budget/controller/GetAmountControllerTest.java
@@ -0,0 +1,16 @@
+package com.cartisan.modern.budget.controller;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetAmountControllerTest {
+
+    @Test
+    public void go_to_get_amount_page() {
+        GetAmountController controller = new GetAmountController();
+        assertEquals("get_amount", controller.getAmount());
+    }
+
+}
