5f74ddf309c731645ec483965f2bdbcc0ff280a1
colin <zhang_colin@163.com>
2016-12-21 09:16:40 +0800

    add budget for month is now failing with assertion

---
+++ src/main/java/com/cartisan/modern/budget/controller/AddBudgetForMonthController.java
@@ -0,0 +1,12 @@
+package com.cartisan.modern.budget.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddBudgetForMonthController {
+    @RequestMapping("/confirm_add_budget_for_month")
+    public String confirm(){
+        return "add_budget_for_month";
+    }
+}
---
+++ src/test/java/com/cartisan/modern/budget/controller/AddBudgetForMonthControllerTest.java
@@ -0,0 +1,14 @@
+package com.cartisan.modern.budget.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddBudgetForMonthControllerTest {
+    @Test
+    public void go_to_add_budget_for_month_page(){
+        AddBudgetForMonthController controller = new AddBudgetForMonthController();
+
+        assertEquals("add_budget_for_month", controller.confirm());
+    }
+}
