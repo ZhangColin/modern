ed2b0839bc4b98eab25f75603e83676582596312
colin <zhang_colin@163.com>
2016-12-21 09:06:40 +0800

    add budget for month is now failing with assertion

--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
@@ -1,14 +1,26 @@
 package com.cartisan.modern.acceptancetest.steps;
 
 
+import com.cartisan.modern.budget.MonthlyBudgetRepository;
 import cucumber.api.java8.En;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 public class AddMonthlyBudgetSteps implements En {
     private WebDriver driver;
+
+    @Autowired
+            private MonthlyBudgetRepository monthlyBudgetRepository;
+
     {
         When("^add budget for \"([^\"]*)\" with amount (\\d+)$", (String month, Integer budget) -> {
             driver = new FirefoxDriver();
@@ -20,7 +32,21 @@ public class AddMonthlyBudgetSteps implements En {
             budgetTextBox.submit();
         });
 
-        Then("^monthly budget (\\d+) for \"([^\"]*)\" is saved$", (Integer arg1, String arg2) -> {
+        Then("^monthly budget (\\d+) for \"([^\"]*)\" is saved$", (Integer budget, String month) -> {
+            assertEquals(1, monthlyBudgetRepository.count());
+
+            monthlyBudgetRepository.findAll().forEach(monthlyBudget -> {
+                Date monthDate = null;
+                try{
+                    monthDate = new SimpleDateFormat("yyyy-MM").parse(month);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                assertEquals(monthDate, monthlyBudget.getMonth());
+                assertEquals(budget, monthlyBudget.getBudget());
+            });
+
             driver.close();
         });
 
