121fcbd0fa9ecaccc7ba6120a0d793759ae91c18
colin <zhang_colin@163.com>
2016-12-21 08:56:23 +0800

    monthly budget can now be added via page without save

--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
@@ -2,16 +2,26 @@ package com.cartisan.modern.acceptancetest.steps;
 
 
 import cucumber.api.java8.En;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Created by colin on 2016/12/20.
- */
 public class AddMonthlyBudgetSteps implements En {
+    private WebDriver driver;
     {
-        When("^add budget for \"([^\"]*)\" with amount (\\d+)$", (String arg1, Integer arg2) -> {
+        When("^add budget for \"([^\"]*)\" with amount (\\d+)$", (String month, Integer budget) -> {
+            driver = new FirefoxDriver();
+            driver.get("http://localhost:8080/add_budget_for_month");
+            WebElement monthTextBox = driver.findElement(By.name("month"));
+            monthTextBox.sendKeys(month);
+            WebElement budgetTextBox = driver.findElement(By.name("budget"));
+            budgetTextBox.sendKeys(String.valueOf(budget));
+            budgetTextBox.submit();
         });
 
         Then("^monthly budget (\\d+) for \"([^\"]*)\" is saved$", (Integer arg1, String arg2) -> {
+            driver.close();
         });
 
     }
