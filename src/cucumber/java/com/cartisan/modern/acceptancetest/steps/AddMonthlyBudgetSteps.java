56a3eebe3c64377431c586f74963fb5ed9a7fad2
colin <zhang_colin@163.com>
2016-12-22 19:25:17 +0800

    add budget for month which was set a budget already. This scenario now works correctly

--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/AddMonthlyBudgetSteps.java
@@ -1,6 +1,7 @@
 package com.cartisan.modern.acceptancetest.steps;
 
 
+import com.cartisan.modern.budget.MonthlyBudget;
 import com.cartisan.modern.budget.MonthlyBudgetRepository;
 import cucumber.api.java8.En;
 import org.openqa.selenium.By;
@@ -23,33 +24,59 @@ public class AddMonthlyBudgetSteps implements En {
 
     {
         When("^add budget for \"([^\"]*)\" with amount (\\d+)$", (String month, Integer budget) -> {
-            driver = new FirefoxDriver();
-            driver.get("http://localhost:8080/add_budget_for_month");
-            WebElement monthTextBox = driver.findElement(By.name("month"));
-            monthTextBox.sendKeys(month);
-            WebElement budgetTextBox = driver.findElement(By.name("budget"));
-            budgetTextBox.sendKeys(String.valueOf(budget));
-            budgetTextBox.submit();
+            addBudgetForMonth(month, budget);
         });
 
         Then("^monthly budget (\\d+) for \"([^\"]*)\" is saved$", (Integer budget, String month) -> {
-            assertEquals(1, monthlyBudgetRepository.count());
-
-            monthlyBudgetRepository.findAll().forEach(monthlyBudget -> {
-                Date monthDate = null;
-                try {
-                    monthDate = new SimpleDateFormat("yyyy-MM").parse(month);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-                assertEquals(monthDate, monthlyBudget.getMonth());
-                assertEquals(budget, monthlyBudget.getBudget());
-            });
-
-            monthlyBudgetRepository.deleteAll();
-            driver.close();
+            verifyMonthlyBudgetIsSaved(budget, month);
         });
 
+        Given("^budget (\\d+) has been set for month \"([^\"]*)\"$", (Integer budget, String month) -> {
+            Date monthDate = null;
+            try {
+                monthDate = new SimpleDateFormat("yyyy-MM").parse(month);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            monthlyBudgetRepository.save(new MonthlyBudget(monthDate, budget));
+        });
+
+        When("^add budget for \"([^\"]*)\" with a new amount (\\d+)$", (String month, Integer budget) -> {
+            addBudgetForMonth(month, budget);
+        });
+
+        Then("^the budget for \"([^\"]*)\" is (\\d+)$", (String month, Integer budget) -> {
+            verifyMonthlyBudgetIsSaved(budget, month);
+        });
+    }
+
+    private void verifyMonthlyBudgetIsSaved(Integer budget, String month) {
+        assertEquals(1, monthlyBudgetRepository.count());
+
+        monthlyBudgetRepository.findAll().forEach(monthlyBudget -> {
+            Date monthDate = null;
+            try {
+                monthDate = new SimpleDateFormat("yyyy-MM").parse(month);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            assertEquals(monthDate, monthlyBudget.getMonth());
+            assertEquals(budget, monthlyBudget.getBudget());
+        });
+
+        monthlyBudgetRepository.deleteAll();
+        driver.close();
+    }
+
+    private void addBudgetForMonth(String month, Integer budget) {
+        driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/add_budget_for_month");
+        WebElement monthTextBox = driver.findElement(By.name("month"));
+        monthTextBox.sendKeys(month);
+        WebElement budgetTextBox = driver.findElement(By.name("budget"));
+        budgetTextBox.sendKeys(String.valueOf(budget));
+        budgetTextBox.submit();
     }
 }
