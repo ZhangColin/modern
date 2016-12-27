f7eec6b2c2ca75ef58b1bb238fe67ffed284a0e4
colin <zhang_colin@163.com>
2016-12-27 18:11:05 +0800

    create a skeleton of scenario:add a nnew transaction

---
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/Transaction.java
@@ -0,0 +1,11 @@
+package com.cartisan.modern.acceptancetest.steps;
+
+import lombok.Data;
+
+@Data
+public class Transaction {
+    private String type;
+    private String description;
+    private String date;
+    private Integer amount;
+}
