22b5154dbb909d7984a9ee02d7ce940ef51c81b1
colin <zhang_colin@163.com>
2016-12-28 10:48:30 +0800

    move spring integration test annotations to Hooks

--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/Hooks.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/Hooks.java
@@ -1,11 +1,22 @@
 package com.cartisan.modern.acceptancetest.steps;
 
+import com.cartisan.modern.Application;
 import com.cartisan.modern.acceptancetest.driver.UiDriver;
 import com.cartisan.modern.budget.MonthlyBudgetRepository;
 import com.cartisan.modern.user.UserRepository;
 import cucumber.api.java.After;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
+@WebAppConfiguration
+@IntegrationTest
 public class Hooks {
     @Autowired
     private UiDriver uiDriver;
--- src/cucumber/java/com/cartisan/modern/acceptancetest/steps/LoginSteps.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/LoginSteps.java
@@ -1,29 +1,17 @@
 package com.cartisan.modern.acceptancetest.steps;
 
-import com.cartisan.modern.Application;
 import com.cartisan.modern.acceptancetest.driver.UiDriver;
 import com.cartisan.modern.user.User;
 import com.cartisan.modern.user.UserRepository;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationContextLoader;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-@WebAppConfiguration
-@IntegrationTest
 public class LoginSteps{
 
     @Autowired
