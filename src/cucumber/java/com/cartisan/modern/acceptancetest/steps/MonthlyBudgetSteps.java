c735b45ee1312abb3e3c9bf3b55f94211a592a57
colin <zhang_colin@163.com>
2016-12-28 17:50:55 +0800

    merge monthly budget steps. and fix a defect with then step

---
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/steps/MonthlyBudgetSteps.java
@@ -0,0 +1,79 @@
+package com.cartisan.modern.acceptancetest.steps;
+
+
+import com.cartisan.modern.acceptancetest.pages.AddMonthlyBudgetPage;
+import com.cartisan.modern.acceptancetest.pages.CommonPage;
+import com.cartisan.modern.acceptancetest.pages.MonthlyBudgetAmountPage;
+import com.cartisan.modern.budget.MonthlyBudget;
+import com.cartisan.modern.budget.MonthlyBudgetRepository;
+import cucumber.api.Format;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+import static com.cartisan.modern.acceptancetest.steps.Formats.MONTH;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MonthlyBudgetSteps {
+    @Autowired
+    private MonthlyBudgetAmountPage monthlyBudgetAmountPage;
+
+    @Autowired
+    private AddMonthlyBudgetPage addMonthlyBudgetPage;
+
+    @Autowired
+    private CommonPage commonPage;
+
+    @Autowired
+    private MonthlyBudgetRepository monthlyBudgetRepository;
+
+    @Given("^budget (\\d+) has been set for month \"([^\"]*)\"$")
+    public void budget_has_been_set_for_month(Integer budget, @Format(MONTH)Date month) {
+        monthlyBudgetRepository.save(new MonthlyBudget(month, budget));
+    }
+
+    @Given("^budget planned for \"([^\"]*)\" is (\\d+)$")
+    public void budget_planned_for_is(@Format(MONTH)Date month, Integer budget) {
+        budget_has_been_set_for_month(budget, month);
+    }
+
+    @When("^add budget for \"([^\"]*)\" with amount (\\d+)$")
+    public void add_budget_for_with_amount(String month, Integer budget) {
+        addMonthlyBudgetPage.addMonthlyBudget(month, budget);
+    }
+
+    @When("^add budget for \"([^\"]*)\" with a new amount (\\d+)$")
+    public void add_budget_for_with_a_new_amount(String month, Integer budget) {
+        add_budget_for_with_amount(month, budget);
+    }
+
+    @Then("^monthly budget (\\d+) for \"([^\"]*)\" is saved$")
+    public void monthly_budget_for_is_saved(Integer budget, @Format(MONTH)Date month) {
+        assertEquals(1, monthlyBudgetRepository.count());
+
+        monthlyBudgetRepository.findAll().forEach(monthlyBudget -> {
+            assertEquals(month, monthlyBudget.getMonth());
+            assertEquals(budget, monthlyBudget.getBudget());
+        });
+    }
+
+    @Then("^the budget for \"([^\"]*)\" is (\\d+)$")
+    public void the_budget_for_with_a_new_amount(@Format(MONTH)Date month, Integer budget) {
+        monthly_budget_for_is_saved(budget, month);
+    }
+
+    @When("^get amount of period from \"([^\"]*)\" to \"([^\"]*)\"$")
+    public void get_amount_of_period_from_to(String startDate, String endDate) {
+        monthlyBudgetAmountPage.open(startDate, endDate);
+    }
+
+    @Then("^the amount is (\\d+)$")
+    public void the_amount_is(Integer amount) {
+        assertTrue(commonPage.getAllText().contains(String.valueOf(amount)));
+    }
+
+}
