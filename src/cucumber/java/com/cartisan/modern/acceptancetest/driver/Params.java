5e80162b84e83431b89fef8c3f15cdce6cf157b9
colin <zhang_colin@163.com>
2016-12-28 16:54:09 +0800

    Params.getQuery:empty query and one param

---
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/driver/Params.java
@@ -0,0 +1,18 @@
+package com.cartisan.modern.acceptancetest.driver;
+
+public class Params {
+    private String name;
+    private String value;
+
+    public void add(String name, String value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getQuery(){
+        if (name==null)
+            return "";
+
+        return String.format("?%s=%s", name, value);
+    }
+}
---
+++ src/test/java/com/cartisan/modern/acceptancetest/driver/ParamsQueryTest.java
@@ -0,0 +1,21 @@
+package com.cartisan.modern.acceptancetest.driver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParamsQueryTest {
+    Params params = new Params();
+
+    @Test
+    public void empty_query_when_there_is_no_param(){
+        assertEquals("", params.getQuery());
+    }
+
+    @Test
+    public void query_string_when_there_is_one_param(){
+        params.add("name", "value");
+
+        assertEquals("?name=value", params.getQuery());
+    }
+}
