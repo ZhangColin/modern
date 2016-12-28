94681d7d984ed26942fc1dc1720704135cd007db
colin <zhang_colin@163.com>
2016-12-28 17:12:27 +0800

    Params.getQuery:two params

--- src/cucumber/java/com/cartisan/modern/acceptancetest/driver/Params.java
+++ src/cucumber/java/com/cartisan/modern/acceptancetest/driver/Params.java
@@ -1,18 +1,30 @@
 package com.cartisan.modern.acceptancetest.driver;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.joining;
+
 public class Params {
-    private String name;
-    private String value;
+    public static final String PARAM_CONNECTOR = "=";
+    public static final String QUERY_HEAD = "?";
+    public static final String PARAM_DELIMITER = "&";
+    private final Map<String, String> params = new HashMap<>();
 
     public void add(String name, String value){
-        this.name = name;
-        this.value = value;
+        params.put(name, value);
     }
 
     public String getQuery(){
-        if (name==null)
+        if (params.isEmpty())
             return "";
 
-        return String.format("?%s=%s", name, value);
+        return QUERY_HEAD +params.entrySet().stream()
+                .map(this::paramInQuery)
+                .collect(joining(PARAM_DELIMITER));
+    }
+
+    private String paramInQuery(Map.Entry<String, String> param) {
+        return param.getKey()+ PARAM_CONNECTOR +param.getValue();
     }
 }
