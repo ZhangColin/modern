28ef0d27c3e6dc8f19beaad5ec5ad33be2b0f331
colin <zhang_colin@163.com>
2017-01-11 11:36:28 +0800

    validation: will show all transaction types when has field error

--- src/test/java/com/cartisan/modern/transaction/controller/TransactionControllerValidTest.java
+++ src/test/java/com/cartisan/modern/transaction/controller/TransactionControllerValidTest.java
@@ -12,30 +12,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class TransactionControllerValidTest {
-        Model stubModel = mock(Model.class);
-        Transaction transaction = new Transaction();
-        BindingResult stubBindingResult = mock(BindingResult.class);
-        Transactions mockTransactions = mock(Transactions.class);
+    Model mockModel = mock(Model.class);
+    Transaction transaction = new Transaction();
+    BindingResult stubBindingResult = mock(BindingResult.class);
+    Transactions mockTransactions = mock(Transactions.class);
     TransactionController controller = new TransactionController(mockTransactions);
 
     @Before
-    public void givenHasFieldErrors(){
+    public void givenHasFieldErrors() {
         when(stubBindingResult.hasFieldErrors()).thenReturn(true);
     }
 
     @Test
-    public void will_not_add_transaction_when_has_field_error(){
+    public void will_not_add_transaction_when_has_field_error() {
         submitTransactionAdd();
 
         verify(mockTransactions, never()).add(transaction);
     }
 
     @Test
-    public void will_go_to_add_transaction_page_when_has_field_error(){
+    public void will_go_to_add_transaction_page_when_has_field_error() {
         assertThat(submitTransactionAdd()).isEqualTo(TRANSACTION_ADD);
     }
 
+    @Test
+    public void will_show_all_transaction_types_when_has_field_error(){
+        submitTransactionAdd();
+
+        verify(mockModel).addAttribute("types", Transaction.Type.values());
+    }
+
+
     private String submitTransactionAdd() {
-        return controller.submitAddTransaction(transaction, stubBindingResult, stubModel);
+        return controller.submitAddTransaction(transaction, stubBindingResult, mockModel);
     }
 }
