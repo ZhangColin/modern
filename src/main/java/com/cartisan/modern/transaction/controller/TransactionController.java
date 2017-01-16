03022d1474a93950778de9b9e50123a72d352643
colin <zhang_colin@163.com>
2017-01-16 11:47:35 +0800

    show all transactions: controller now gets the data from transactions

--- src/main/java/com/cartisan/modern/transaction/controller/TransactionController.java
+++ src/main/java/com/cartisan/modern/transaction/controller/TransactionController.java
@@ -14,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
+import static com.cartisan.modern.common.Formats.DAY;
 import static com.cartisan.modern.common.controller.ControllerHelper.setMessage;
 import static com.cartisan.modern.common.controller.Urls.TRANSACTION_ADD;
 import static com.cartisan.modern.common.controller.Urls.TRANSACTION_LIST;
-import static java.util.Arrays.asList;
 
 @Controller
 @PropertySource("classpath:resultMessages.properties")
@@ -53,19 +56,23 @@ public class TransactionController {
 
     @RequestMapping(value = TRANSACTION_LIST, method = RequestMethod.GET)
     public String showTransactions(Model model) {
-        model.addAttribute("transactions", asList(
-                presentableTransaction(Transaction.Type.Income, "Course Registration", "2016-08-14", 4000),
-                presentableTransaction(Transaction.Type.Outcome, "Buy MacBook Pro", "2015-11-01", 100)
-        ));
+        model.addAttribute("transactions", allTransactions());
         return TRANSACTION_LIST;
     }
 
-    private PresentableTransaction presentableTransaction(Transaction.Type type, String description, String date, int amount) {
+    private List<PresentableTransaction> allTransactions() {
+        List<PresentableTransaction> all = new ArrayList<>();
+        transactions.processAll(transaction -> all.add(presentableTransaction(transaction)));
+
+        return all;
+    }
+
+    private PresentableTransaction presentableTransaction(Transaction transaction) {
         PresentableTransaction presentableTransaction = new PresentableTransaction();
-        presentableTransaction.setType(type);
-        presentableTransaction.setDescription(description);
-        presentableTransaction.setDate(date);
-        presentableTransaction.setAmount(amount);
+        presentableTransaction.setType(transaction.getType());
+        presentableTransaction.setDescription(transaction.getDescription());
+        presentableTransaction.setDate(new SimpleDateFormat(DAY).format(transaction.getDate()));
+        presentableTransaction.setAmount(transaction.getAmount());
 
         return presentableTransaction;
     }
