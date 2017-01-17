e37d479b252c991bf2ee642d06f424ee1e4f82da
colin <zhang_colin@163.com>
2017-01-17 17:54:10 +0800

    refactoring: introduce sign in view object to separate code

--- src/test/java/com/cartisan/modern/common/controller/AuthenticationFailedTest.java
+++ src/test/java/com/cartisan/modern/common/controller/AuthenticationFailedTest.java
@@ -1,5 +1,6 @@
 package com.cartisan.modern.common.controller;
 
+import com.cartisan.modern.common.view.SignIn;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -7,20 +8,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class AuthenticationFailedTest {
-    private SessionController controller = new SessionController();
+    private SignIn signIn = new SignIn();
 
     @Test
     public void back_to_sign_in(){
-        ModelAndView actual = controller.signIn(null, null);
+        ModelAndView actual = signIn.getModel(null, null);
 
         assertEquals("signin", actual.getViewName());
     }
 
     @Test
     public void message_when_sign_in_error(){
-        controller.failedMessage = "a failed message";
+        signIn.failedMessage = "a failed message";
 
-        ModelAndView actual = controller.signIn("any error", null);
+        ModelAndView actual = signIn.getModel("any error", null);
 
         assertThat(actual.getModel().get("message")).isEqualTo("a failed message");
         assertThat(actual.getModel().get("type")).isEqualTo("danger");
@@ -29,9 +30,9 @@ public class AuthenticationFailedTest {
 
     @Test
     public void message_when_logout(){
-        controller.logoutMessage = "a logout message";
+        signIn.logoutMessage = "a logout message";
 
-        ModelAndView actual = controller.signIn(null, "something logout");
+        ModelAndView actual = signIn.getModel(null, "something logout");
 
         assertThat(actual.getModel().get("message")).isEqualTo("a logout message");
         assertThat(actual.getModel().get("type")).isEqualTo("info");
