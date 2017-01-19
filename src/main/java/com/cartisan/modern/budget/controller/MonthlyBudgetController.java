38e62de08dd02df3e3acf07c9daaf2885a6f93c5
colin <zhang_colin@163.com>
2017-01-19 19:16:16 +0800

    refactoring: simplify request mapping and url

--- src/main/java/com/cartisan/modern/budget/controller/MonthlyBudgetController.java
+++ src/main/java/com/cartisan/modern/budget/controller/MonthlyBudgetController.java
@@ -10,22 +10,20 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Date;
 
 import static com.cartisan.modern.common.Formats.DAY;
 import static com.cartisan.modern.common.controller.ControllerHelper.thenSetMessage;
-import static com.cartisan.modern.common.controller.Urls.MONTHLYBUDGET_ADD;
-import static com.cartisan.modern.common.controller.Urls.MONTHLYBUDGET_TOTALAMOUNT;
+import static com.cartisan.modern.common.controller.Urls.*;
 import static com.cartisan.modern.common.view.Messages.RESULT_MESSAGES_FULL_NAME;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
+@RequestMapping(MONTHLYBUDGET)
 @PropertySource(RESULT_MESSAGES_FULL_NAME)
 public class MonthlyBudgetController {
     private final MonthlyBudgetPlanner planner;
@@ -43,7 +41,7 @@ public class MonthlyBudgetController {
         this.presentableMonthlyBudgetAmount = presentableMonthlyBudgetAmount;
     }
 
-    @RequestMapping(value = MONTHLYBUDGET_ADD, method = POST)
+    @PostMapping(ADD)
     public String submitAddMonthlyBudget(@Valid @ModelAttribute MonthlyBudget monthlyBudget, BindingResult result, Model model) {
         if (!result.hasFieldErrors())
             planner.addMonthlyBudget(monthlyBudget)
@@ -53,12 +51,12 @@ public class MonthlyBudgetController {
         return addMonthlyBudget();
     }
 
-    @RequestMapping(value = MONTHLYBUDGET_ADD, method = GET)
+    @GetMapping(ADD)
     public String addMonthlyBudget() {
         return MONTHLYBUDGET_ADD;
     }
 
-    @RequestMapping(value = MONTHLYBUDGET_TOTALAMOUNT, method = GET)
+    @GetMapping(TOTALAMOUNT)
     public String totalAmountOfMonthlyBudget(
             @RequestParam("startDate") @DateTimeFormat(pattern = DAY) Date startDate,
             @RequestParam("endDate") @DateTimeFormat(pattern = DAY) Date endDate) {
