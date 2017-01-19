da82108c4c4e352fe74331b9ca229b050bc186ff
colin <zhang_colin@163.com>
2017-01-19 18:16:11 +0800

    refactoring: common label text now moves to label text message

--- src/main/java/com/cartisan/modern/transaction/view/PresentableAddTransaction.java
+++ src/main/java/com/cartisan/modern/transaction/view/PresentableAddTransaction.java
@@ -1,7 +1,6 @@
 package com.cartisan.modern.transaction.view;
 
 import com.cartisan.modern.transaction.domain.Transaction;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
@@ -11,11 +10,7 @@ import static com.cartisan.modern.common.view.Messages.RESULT_MESSAGES_FULL_NAME
 @Component
 @PropertySource(RESULT_MESSAGES_FULL_NAME)
 public class PresentableAddTransaction {
-    @Value("${label.add}")
-    String add;
-
     public void display(Model model, Transaction.Type[] values) {
         model.addAttribute("types", values);
-        model.addAttribute("label.add", add);
     }
 }
