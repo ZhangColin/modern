2c117edede9ffdc6651c9b920f55777dd3958182
colin <zhang_colin@163.com>
2017-02-03 11:20:20 +0800

    refactoring: delete unused code of set hidden in presentable transactions

--- src/test/java/com/cartisan/modern/transaction/view/PresentableTransactionsTest.java
+++ src/test/java/com/cartisan/modern/transaction/view/PresentableTransactionsTest.java
@@ -28,11 +28,6 @@ public class PresentableTransactionsTest {
 
     public class NoTransaction {
         @Test
-        public void should_hide_list_view() {
-            assertThat(modelOf(presentableTransactions).get("hidden")).isEqualTo("hidden");
-        }
-
-        @Test
         public void should_show_message() {
             presentableTransactions = new PresentableTransactions("no transaction message");
 
@@ -44,13 +39,6 @@ public class PresentableTransactionsTest {
     public class HasSomeTransactions {
         private Date date = parseDay("2016-07-01");
         private int amount = 100;
-
-        @Test
-        public void should_not_hide_list_view() {
-            displayTransaction();
-
-            assertThat(modelOf(presentableTransactions)).doesNotContainKey("hidden");
-        }
 
         @Test
         public void should_not_show_message() {
